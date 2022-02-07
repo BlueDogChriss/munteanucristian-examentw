a950ad2d677bf685cb5bd35b287a7893472ae1f7
Cristian Munteanu <munteanu.cristian1009@gmail.com>
2022-02-07 09:59:11 +0200

    initial commit , server and packages, react

---
+++ back/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = require("./routes");
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 8081;
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended:true}));
+app.use(express.static(path.join(__dirname, 'build')));
+app.use("/api", router);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
